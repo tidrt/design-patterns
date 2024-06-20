82ee5e105e2fff1b3be56279ee70ec90dd90ad97
tiago <tigdrt@gmail.com>
2024-06-20 16:59:34 -0300

    singleton: solving with lazy singleton

---
+++ design-patterns/src/main/java/com/studies/exercises/singleton/solution/AgendaSingletonLAZY.java
@@ -0,0 +1,38 @@
+package com.studies.exercises.singleton.solution;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class AgendaSingletonLAZY {
+
+    private static AgendaSingletonLAZY INSTANCE = null;
+
+    private Map<String, Boolean> availableDays = new HashMap<>();
+
+    public AgendaSingletonLAZY(){
+        availableDays.put("Segunda", true);
+        availableDays.put("Terça", true);
+        availableDays.put("Quarta", true);
+        availableDays.put("Quinta", true);
+        availableDays.put("Sexta", true);
+        availableDays.put("Sabado", true);
+        availableDays.put("Domingo", true);
+    }
+
+    public static AgendaSingletonLAZY getInstance(){
+        if(Objects.isNull(INSTANCE)){
+            INSTANCE = new AgendaSingletonLAZY();
+            return INSTANCE;
+        }
+        return INSTANCE;
+    }
+
+    public Map<String, Boolean> getDays(){
+        return availableDays;
+    }
+
+    public void toOcuppy(String string){
+        availableDays.replace(string, Boolean.FALSE);
+    }
+}
--- design-patterns/src/main/java/com/studies/exercises/singleton/solution/AgendaTestWithSingleton.java
+++ design-patterns/src/main/java/com/studies/exercises/singleton/solution/AgendaTestWithSingleton.java
@@ -4,14 +4,22 @@ import com.studies.exercises.singleton.problem.Agenda;
 
 public class AgendaTestWithSingleton {
     public static void main(String[] args) {
+        // reserveEAGER("Sexta");
+        // reserveEAGER("Sabado");
 
-        reserveEAGER("Sexta");
-        reserveEAGER("Sabado");
+        reserveLAZY("Sexta");
+        reserveLAZY("Sabado");
 
     }
 
     public static void reserveEAGER(String day){
         AgendaSingletonEAGER agenda = AgendaSingletonEAGER.getInstance();
+        agenda.toOcuppy(day);
+        System.out.println(agenda.getDays());
+    }
+
+    public static void reserveLAZY(String day){
+        AgendaSingletonLAZY agenda = AgendaSingletonLAZY.getInstance();
         agenda.toOcuppy(day);
         System.out.println(agenda.getDays());
     }
